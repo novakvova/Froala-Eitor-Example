739416cdafeba83581d0ac268790addd1366594a
novakvova <novakvova@gmail.com>
2018-09-15 09:50:27 +0300

    Add project files.

---
+++ SiteProduct/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SiteProduct.Startup))]
+namespace SiteProduct
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

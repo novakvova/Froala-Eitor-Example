4720cb8f25f2cb912354cbec81dbf98a2f05c467
novakvova <novakvova@gmail.com>
2018-09-22 12:09:07 +0300

    Delete image in foala  good

--- SiteProduct/Controllers/ProductController.cs
+++ SiteProduct/Controllers/ProductController.cs
@@ -2,6 +2,9 @@
 using SiteProduct.Core;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -23,9 +26,47 @@ namespace SiteProduct.Controllers
         public JsonResult UploadImageDecription(HttpPostedFileBase file)
         {
             string link = string.Empty;
-            string path = Url.Content(Constants.ProductDescriptionPath);
-
-            return Json(new { link = $"{path}default.jpg" });
+            string filename= Guid.NewGuid().ToString() + ".jpg";
+            string image = Server.MapPath(Constants.ProductDescriptionPath) +
+                filename;
+            try
+            {
+                using (Bitmap bmp = new Bitmap(file.InputStream))
+                {
+                    var saveImage = ImageWorker.CreateImage(bmp, 450, 450);
+                    if(saveImage!=null)
+                    {
+                        saveImage.Save(image, ImageFormat.Jpeg);
+                        link = Url.Content(Constants.ProductDescriptionPath) + 
+                            filename;
+                    }
+                }
+                string path = Url.Content(Constants.ProductDescriptionPath);
+            }
+            catch (Exception)
+            {
+                if(System.IO.File.Exists(image))
+                {
+                    System.IO.File.Delete(image);
+                }
+            }
+            return Json(new { link });
         }
+
+        [HttpPost]
+        public JsonResult DeleteImageDecription(string src)
+        {
+            string link = string.Empty;
+            string filename = Path.GetFileName(src);
+            string image = Server.MapPath(Constants.ProductDescriptionPath) +
+                filename;
+
+            if (System.IO.File.Exists(image))
+            {
+                System.IO.File.Delete(image);
+            }
+            return Json(new { link });
+        }
+        
     }
 }
---
+++ SiteProduct/DAL/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SiteProduct.DAL.Entities
+{
+    [Table("tblProducts")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, StringLength(255)]
+        public string Name { get; set; }
+        [StringLength(4000)]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
